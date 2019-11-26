ca47796cfdffd76d1e5da6b6643c77ea11c55516
ricardo <ricardoroque98@gmail.com>
2019-11-26 20:45:59 +0000

    inicio dos testes unitarios

---
+++ src/testes/modelo/ModeloTest.java
@@ -0,0 +1,82 @@
+package modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModeloTest {
+
+    @org.junit.jupiter.api.Test
+    void getSaldoUtilizador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void login() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void gettipoUtilizador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void logout() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFavorito() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFavoritos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFavorito() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSenhasCompradas() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRefeicao() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buySenha() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cancelSenha() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeSenha() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNovoSaldoUtilizador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addUtilizador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeUtilizador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addNovaRefeicao() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeRefeicao() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cancelRefeicao() {
+    }
+}
