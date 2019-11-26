2ba5e95a915d4e82dae8160cc2c24da8280714e9
rafaelsa99 <rafa.sa_@hotmail.com>
2019-11-26 04:18:04 +0000

    gerir senhas (falta handle correto dos botões)

--- src/vistas/GerirSenhasController.java
+++ src/vistas/GerirSenhasController.java
@@ -1,7 +1,9 @@
 package vistas;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -11,13 +13,18 @@ import modelo.Senha;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class GerirSenhasController {
 
     PaneOrganizer po;
     private ArrayList<Senha> senhas;
     @FXML private TableView tableSenhas;
+    @FXML TableColumn<TableSenha, Integer> colNumero;
+    @FXML TableColumn<TableSenha, String> colPrato;
+    @FXML TableColumn<TableSenha, String> colSobremesa;
+    @FXML TableColumn<TableSenha, Double> colPreco;
+    @FXML TableColumn<TableSenha, Button> colAlterar;
+    @FXML TableColumn<TableSenha, Button> colCancelar;
 
     public GerirSenhasController(PaneOrganizer paneOrganizer) {
         po=paneOrganizer;
@@ -36,10 +43,31 @@ public class GerirSenhasController {
 
     public void initialize() {
         tableSenhas.setEditable(false);
+        colNumero.setCellValueFactory(new PropertyValueFactory<>("IdSenha"));
+        colPrato.setCellValueFactory(new PropertyValueFactory<>("Prato"));
+        colSobremesa.setCellValueFactory(new PropertyValueFactory<>("Sobremesa"));
+        colPreco.setCellValueFactory(new PropertyValueFactory<>("Preco"));
+        colAlterar.setCellValueFactory(new PropertyValueFactory<>("BtAlterar"));
+        colCancelar.setCellValueFactory(new PropertyValueFactory<>("BtCancelar"));
+
         try {
             senhas = po.getControlador().getSenhasCompradas();
             for (Senha senha : senhas) {
-                //tableSenhas.getItems().add(senha);
+                TableSenha tbSenha=new TableSenha(senha.getIdSenha(), senha.getPrato(), senha.getSombremesa(), senha.getPreco(), senha.getIdRefeicao());
+                tbSenha.getBtAlterar().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        System.out.println("Alterar" + tbSenha.getBtAlterar().getId());
+                    }
+                });
+                tbSenha.getBtCancelar().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        System.out.println("Cancelar " + tbSenha.getBtAlterar().getId());
+                    }
+                });
+                tableSenhas.getItems().add(tbSenha);
+
             }
         } catch (SQLException e) {
             tableSenhas.setPlaceholder(new Label(e.getMessage().toString()));
